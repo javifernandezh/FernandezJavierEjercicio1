58b7c2cb96c21df343465e908a7a69eecd1a5086
javifernandezh <javifdez6@gmail.com>
2026-02-20 12:01:38 +0100

    Tarea 2
    Solicitar al usuario que introduzca su decimo y el importe, que los numeros de los boletos sean de 5 cifras y si no que se completen con 0 a la izquierda, ademas que no pueda introducir letras o caracteres en comprobacion de boletos o importe. Comprobar si el usuario ha ganado algun premio y multiplicarlo por la cantidad de boletos comprados.

--- FernandezJavierEjercicio1.java
+++ FernandezJavierEjercicio1.java
@@ -1,16 +1,63 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class FernandezJavierEjercicio1 {
     public static void main(String[] args) {
         Random r = new Random();
+        Scanner sc = new Scanner(System.in);
 
+        // Tarea 1
         int primerPremio = r.nextInt(99999) + 1;
         int segundoPremio = r.nextInt(99999) + 1;
         int tercerPremio = r.nextInt(99999) + 1;
 
         System.out.println("Sorteo Lotería de Navidad:");
-        System.out.println("Primer premio: " + String.format("%05d", primerPremio) + " -> 20.000€ por € jugado");
-        System.out.println("Segundo premio: " + String.format("%05d", segundoPremio) + " -> 6.250€ por € jugado");
-        System.out.println("Tercer premio: " + String.format("%05d", tercerPremio) + " -> 2.500€ por € jugado");
+        System.out.println("Primer premio: " + String.format("%05d", primerPremio));
+        System.out.println("Segundo premio: " + String.format("%05d", segundoPremio));
+        System.out.println("Tercer premio: " + String.format("%05d", tercerPremio));
+
+        // Tarea 2
+        int decimo;
+        while (true) {
+            System.out.print("Introduce el número de tu décimo (1-99999): ");
+            if (sc.hasNextInt()) {
+                decimo = sc.nextInt();
+                if (decimo >= 1 && decimo <= 99999) break;
+                else System.out.println("Error: el número debe estar entre 1 y 99999.");
+            } else {
+                System.out.println("Error: debes introducir un número entero.");
+                sc.next();
+            }
+        }
+
+        // Tarea 2
+        int importe;
+        while (true) {
+            System.out.print("Introduce el importe jugado (mínimo 1€): ");
+            if (sc.hasNextInt()) {
+                importe = sc.nextInt();
+                if (importe >= 1) break;
+                else System.out.println("Error: el importe mínimo es 1€.");
+            } else {
+                System.out.println("Error: debes introducir un número entero.");
+                sc.next();
+            }
+        }
+
+        // Tarea 2
+        System.out.println("\nTu décimo: " + String.format("%05d", decimo));
+        System.out.println("Importe jugado: " + importe + "€");
+
+        if (decimo == primerPremio) {
+            System.out.println("¡Primer premio! Has ganado " + (20000 * importe) + "€");
+        } else if (decimo == segundoPremio) {
+            System.out.println("¡Segundo premio! Has ganado " + (6250 * importe) + "€");
+        } else if (decimo == tercerPremio) {
+            System.out.println("¡Tercer premio! Has ganado " + (2500 * importe) + "€");
+        } else {
+            System.out.println("Lo siento, no has tenido suerte");
+        }
+
+        sc.close();
     }
 }
